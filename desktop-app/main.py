8e3ddd728ec8762c83e0601e9fb5c58ad6d4b834
Duke Norsworthy <duke.norsworthy@gmail.com>
2025-06-14 00:30:14 -0400

    nested layouts added

--- desktop-app/main.py
+++ desktop-app/main.py
@@ -1,7 +1,8 @@
 # import sys
 
 from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QWidget, QVBoxLayout, QLabel, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QWidget, QVBoxLayout, QLabel, QPushButton, QTabWidget
+from PyQt6.QtGui import QColor
 # from layout_colorwidget import Color
 
 class MainWindow(QMainWindow):
@@ -9,17 +10,44 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("EQ Loot Log")
-        layout = QVBoxLayout()
+        self.setFixedSize(QSize(1400,800))
+
+        # main layout
+        self.main_layout                = QHBoxLayout()
+        self.fight_navigation_layout    = QVBoxLayout()
+        self.data_layout                = QVBoxLayout()
+
+        # spacing for main layout
+        self.main_layout.setContentsMargins(0,0,0,0)
+        self.main_layout.setSpacing(10)
+
+        # fight nav (left side of app)
+        self.fight_navigation_label     = QLabel("<h1>Fight Nav</h1>")
+        self.fight_navigation_layout.addWidget(self.fight_navigation_label)
+
+        file_name = "Load a log file here."
+        self.log_file_add_button        = QPushButton(file_name)
+        self.fight_navigation_layout.addWidget(self.log_file_add_button)
+
+        # add to main
+        self.main_layout.addLayout(self.fight_navigation_layout, 15)
+
+        # data (right side of app)
+        self.data_label = QLabel("<h1>Data goes here</h1>")
+        self.data_layout.addWidget(self.data_label)
+
+        # add to main
+        self.main_layout.addLayout(self.data_layout, 85)
 
 
-        # widget.setGeometry(50, 50, 1400, 1000)
+        # self.label = QLabel("<h1> This is important text stuff.</h1>")
+        # self.button = QPushButton("Click me!")
+        # self.button.clicked.connect(self.button_clicked)
 
-        self.label = QLabel("<h1> This is important text stuff.</h1>")
-        self.button = QPushButton("Click me!")
-        self.button.clicked.connect(self.button_clicked)
-
-        layout.addWidget(self.label)
-        layout.addWidget(self.button)
+        # layout.addWidget(self.label)
+        # layout.addWidget(self.button)
+        # color = QColor(255, 0 , 0)
+        # self.button.setStyleSheet(f"background-color: {color.name()};")
 
         # self.master = QHBoxLayout()
         # column_1 = QVBoxLayout()
@@ -28,18 +56,17 @@ class MainWindow(QMainWindow):
         # column_1.setWidget(QLabel("column 1"))
         # column_2.setWidget(QLabel("column 2"))
 
-        # self.layout.addLayout(column_1, 15)
-        # self.layout.addLayout(column_2, 85)    
+        # layout.addWidget(column_1, 15)
+        # layout.addWidget(column_2, 85)    
         
-        # widget.setLayout(self.layout)
-        # self.setFixedSize(QSize(1400,800))
+
 
         # self.setCentralWidget(widget)
 
-        window = QWidget()
-        window.setLayout(layout)
+        self.window = QWidget()
+        self.window.setLayout(self.main_layout)
 
-        self.setCentralWidget(window)
+        self.setCentralWidget(self.window)
 
     def button_clicked(self):
         self.label.setText("Clicked!")
